364cab3ccfb7450a647aa14b6b878011e573f2bd
Lusine Hovhan <lusine.hovhan@yahoo.com>
2021-02-06 16:35:21 +0400

    UpdatedExercise1PrimeNumber

--- src/com/company/Exercise5/Exercise1PrimeNumber.java
+++ src/com/company/Exercise5/Exercise1PrimeNumber.java
@@ -2,6 +2,15 @@ package com.company.Exercise5;
 
 public class Exercise1PrimeNumber {
     public static void main(String[] args) {
+
+        int[] array2 = {1, 8, 2, 97, 9, 1, 17, 25, 37, 14, 0, -15};
+        for(int prime:array2){
+            if(prime == 2 || prime == 3 || prime == 5 || prime == 7 || prime > 10 && prime % 2 != 0 && prime % 3 != 0 && prime % 5 != 0 && prime % 7 != 0) {
+                System.out.print(prime + " ");
+            }
+        }
+        System.out.println();
+
         int[] array = {1, 8, 2, 97, 9, 1, 17, 25, 37, 14, 0, -15};
         for (int i = 0; i < array.length; i++) {
             boolean prime = true;
@@ -19,4 +28,7 @@ public class Exercise1PrimeNumber {
         }
     }
 }
+
+
+
 
