c5358f8a41e9c05ff91473221b5490d3f935e769
Lusine Hovhan <lusine.hovhan@yahoo.com>
2021-02-10 14:48:46 +0400

    Exercise5Updated

--- src/com/company/Exercise6/Exersise5.java
+++ src/com/company/Exercise6/Exersise5.java
@@ -7,17 +7,25 @@ public class Exersise5 {
                 {47, 58, 61, 84},
                 {12, 19, 77, 52},
         };
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[i].length; j++) {
-                System.out.print(mat[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-
+       int p = 0;
+       int m = 0;
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++) {
                 System.out.print(mat[j][i] + " ");
+                p = mat.length;
+                m = mat[i].length;
+            }
+            System.out.println();
+        }
+        int matrix [][] = new int[p][m];
+        System.out.println();
+int k = 0;
+int s = 0;
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < m; j++) {
+                k = i;
+                s = j;
+                System.out.print(mat[k][s] + " ");
             }
             System.out.println();
         }
