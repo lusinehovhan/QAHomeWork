c6772db6d950787cd76dacbae13275564f7f258b
Lusine Hovhan <lusine.hovhan@yahoo.com>
2021-02-11 10:38:36 +0400

    UpdatedExercise5

--- src/com/company/Exercise6/Exersise5.java
+++ src/com/company/Exercise6/Exersise5.java
@@ -22,12 +22,12 @@ public class Exersise5 {
 
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++) {
-                matrix[i][j] = mat[i][j];
+                matrix[j][i] = mat[i][j];
             }
         }
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[j][i]+" ");
+                System.out.print(matrix[i][j]+" ");
             }
             System.out.println();
         }
