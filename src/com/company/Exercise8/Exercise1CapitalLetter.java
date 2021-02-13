0b4bf25c581e536c3869b75922ac2217a4aad9b8
Lusine Hovhan <lusine.hovhan@yahoo.com>
2021-02-13 21:14:32 +0400

    UpdatedExercise8

--- src/com/company/Exercise8/Exercise1CapitalLetter.java
+++ src/com/company/Exercise8/Exercise1CapitalLetter.java
@@ -3,23 +3,20 @@ package com.company.Exercise8;
 public class Exercise1CapitalLetter {
     public static void main(String[] args) {
         String s = "ape JAN Barev";
-        String[] strArr = s.split(" ");
-        String changeLetterSize = "";
-        for (int i = 0; i < strArr.length; i++) {
-            String str = strArr[i];
-            String changeLetter = "";
-            for (int j = 0; j < strArr[i].length(); j++) {
-                if (str.charAt(j) >= 'A' && str.charAt(j) <= 'Z') {
-                    changeLetter = changeLetter + (char) (str.charAt(j) + ' ');
-                }
-                if (str.charAt(j) >= 'a' && str.charAt(j) <= 'z') {
-                    changeLetter = changeLetter + (char) (str.charAt(j) - ' ');
-                }
+        String letter = "";
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                char t = (char) (ch + ' ');
+                letter = letter + t;
+            }else if (ch >= 'a' && ch <= 'z') {
+                letter = letter + (char) (ch - ' ');
+            }else {
+                letter = letter + " ";
             }
-            changeLetterSize = changeLetterSize + changeLetter + " ";
         }
         System.out.println(s);
-        System.out.println(changeLetterSize);
+        System.out.print(letter);
     }}
 
 
