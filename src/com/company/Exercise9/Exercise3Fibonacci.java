c4d4719936dc35a160fa3aea4da723b78758a532
Lusine Hovhan <lusine.hovhan@yahoo.com>
2021-02-16 21:25:53 +0400

    Updated

--- src/com/company/Exercise9/Exercise3Fibonacci.java
+++ src/com/company/Exercise9/Exercise3Fibonacci.java
@@ -2,7 +2,7 @@ package com.company.Exercise9;
 
 public class Exercise3Fibonacci {
     public static void main(String[] args) {
-      fibonacci(12);
+        fibonacci(20);
     }
     static void fibonacci(int count) {
         int n1 = 0;
@@ -10,26 +10,27 @@ public class Exercise3Fibonacci {
         int n3;
         if (count < 0) {
             System.out.println("Negative number. Please, enter positive number.");
+            return;
         }
-        switch (count) {
-            case 0:
-                System.out.println("The entered value returns no result");
-                break;
-            case 1:
-                System.out.print(n1);
-                break;
-            case 2:
-                System.out.print(n1 + " " + n2 + " ");
-                break;
+        if (count == 0) {
+            System.out.println("The entered value returns no result");
+            return;
         }
-        if(count > 2){
-                System.out.print(n1 + " " + n2 + " ");
-                for (int i = 2; i < count; i++) {
-                    n3 = n1 + n2;
-                    System.out.print(n3 + " ");
-                    n1 = n2;
-                    n2 = n3;
-                }
+        if (count == 1) {
+            System.out.print(n1);
+            return;
+        }
+        if (count == 2) {
+            System.out.print(n1 + " " + n2 + " ");
+            return;
+        }
+            System.out.print(n1 + " " + n2 + " ");
+            for (int i = 2; i < count; i++) {
+                n3 = n1 + n2;
+                System.out.print(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
         }
     }
-    }
+
